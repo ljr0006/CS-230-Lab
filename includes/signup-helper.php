ee2019571529f888e725ec9ac9ed5df91979ee19
ljr0006 <ljr0006@mix.wvu.edu>
2020-09-17 21:26:02 -0400

    Signup Page completed (Lab 3)

--- includes/signup-helper.php
+++ includes/signup-helper.php
@@ -1,6 +1,6 @@
 <?php
 
-if(isset($_POST['signup-submit'])){
+if (isset($_POST['signup-submit'])) {
     require 'dbhandler.php';
 
     $username = $_POST['uname'];
@@ -10,48 +10,45 @@ if(isset($_POST['signup-submit'])){
     $passw = $_POST['pwd'];
     $pass_rep = $_POST['con-pwd'];
 
-    if($passw !== $pass_rep){
-        header("Location: ../signup.php?error=diffPasswords&fname=".$fname."&lname=".$lname."&uname=".$username);
+    if ($passw !== $pass_rep) {
+        header("Location: ../signup.php?error=diffPasswords&fname=" . $fname . "&lname=" . $lname . "&uname=" . $username);
         exit();
-    } else{
+    } else {
         $sql = "SELECT uname FROM users WHERE uname=?";
         $stmt = mysqli_stmt_init($conn);
-        if(!mysqli_stmt_prepare($stmt, $sql)){
-            header("Location: ../signup.php?error=SQLInjection&fname=".$fname."&lname=".$lname."&uname=".$username);
+        if (!mysqli_stmt_prepare($stmt, $sql)) {
+            header("Location: ../signup.php?error=SQLInjection");
             exit();
         } else {
             mysqli_stmt_bind_param($stmt, "s", $username);
             mysqli_stmt_execute($stmt);
             mysqli_stmt_store_result($stmt);
             $check = mysqli_stmt_num_rows($stmt);
-            
-            if($check > 0){
-                header("Location: ../signup.php?error=UsernameTaken&fname=".$fname."&lname=".$lname."&uname=".$username);
+
+            if ($check > 0) {
+                header("Location: ../signup.php?error=UsernameTaken");
                 exit();
             } else {
-                $sql = "INSERT INTO user (lname, fname, email, uname, password) VALUES (?, ?, ?, ?, ?)";
+                $sql = "INSERT INTO users (lname, fname, email, uname, password) VALUES (?, ?, ?, ?, ?)";
                 $stmt = mysqli_stmt_init($conn);
-                if(!mysqli_stmt_prepare($stmt, $sql)){
-                    header("Location: ../signup.php?error=SQLInjection&fname=".$fname."&lname=".$lname."&uname=".$username);
+                if (!mysqli_stmt_prepare($stmt, $sql)) {
+                    header("Location: ../signup.php?error=SQLInjection");
                     exit();
                 } else {
                     $hashedPass = password_hash($passw, PASSWORD_BCRYPT);
-                    mysqli_stmt_bind_param($stmt, "sssss",$lname, $fname, $email, $username, $hashedPass);
+                    mysqli_stmt_bind_param($stmt, "sssss", $lname, $fname, $email, $username, $hashedPass);
                     mysqli_stmt_execute($stmt);
                     mysqli_stmt_store_result($stmt);
 
                     header("Location: ../signup.php?signup=success");
-                    exit();
+                    exit();;
                 }
-
             }
         }
         mysqli_stmt_close($stmt);
         mysqli_close($conn);
     }
-
-}
-else {
+} else {
     header("Location: ../signup.php");
     exit();
 }
