c8ce86ef6ce5c3d0bd13cd5ce3bd3ad643ef9d2a
ljr0006 <ljr0006@mix.wvu.edu>
2020-11-05 23:00:01 -0500

    Update Pass Lightsail Lab 8

--- includes/header.php
+++ includes/header.php
@@ -22,7 +22,7 @@ session_start();
 </head>
 <header>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="about.php"><Img src="Images/AvengersLogo.png" style="height: 35px; width: 35px;"></Img> The Avengers</a>
+    <a class="navbar-brand" href="index.php"><Img src="Images/AvengersLogo.png" style="height: 35px; width: 35px;"></Img> The Avengers</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
