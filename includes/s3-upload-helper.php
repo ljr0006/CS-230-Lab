a5d4e2b9517dd34aa3c3601a475da619b44d3a5d
ljr0006 <ljr0006@mix.wvu.edu>
2020-11-10 21:41:14 -0500

    Lab 9 S3

---
+++ includes/s3-upload-helper.php
@@ -0,0 +1,43 @@
+<?php 
+
+require '../vendor/autoload.php';
+
+use Aws\S3\S3Client;
+
+use Aws\Exception\AwsException;
+
+if (isset($_POST['s3-submit'])) {
+    $bucketName = "avengers-s3-cs230";
+    $file = $_FILES['s3-image'];
+    $file_name = $file['name'];
+    $file_tmp_name = $file['tmp_name'];
+    $file_error = $file['error'];
+    $file_size = $file['size'];
+
+    $ext = strtolower(pathinfo($file_name, PATHINFO_EXTENSION));
+
+    try {
+        
+        $s3Client = S3Client::factory(
+            array('region'=>'us-east-2',
+            'version'=>'latest',
+            'credentials'=>array(
+                'key'=> "AKIAWBXS4YGLCUXZA46A",
+                'secret'=> "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"
+            ))
+        );
+        $result = $s3Client->putObject([
+            'Bucket'=>$bucketName,
+            'Key'=>'test_uploads/'.uniqid('',true).'.'.$ext,
+            'SourceFile'=>$file_tmp_name,
+            'ACL'=>'public-read'
+        ]);
+
+        echo 'Success! Photo URL: '.$result->get('ObjectURL');
+
+
+    } catch (Aws\S3\Exception\S3Exception $e) {
+        die('Error Uploading File: '.$e->getMessage());
+    }
+}
+?>
